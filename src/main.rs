e9089b47756920d92f76679f5e3cb89e90fb3f70
Aaron Yu <aaryu103@gmail.com>
2025-11-06 22:17:19 -0800

    add and use sum() function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello, world!");
+    let number= sum(40.9, 59.2);
+    let number_as_string = format!("Sum: {}", number);
+    println!("{}", number_as_string);
+}
+
+fn sum(a: f64, b: f64) -> f64 {
+    return a + b;
 }
